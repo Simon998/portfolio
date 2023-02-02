b8b0821e1506cd8f75263f0ddf524c21c1fed0b1
simon <simonmulu7@gmail.com>
2023-02-02 11:53:09 +0300

    second commit

---
+++ porfolio/main.js
@@ -0,0 +1,18 @@
+
+
+document.querySelector('.header-brand').addEventListener('click', function() {
+  
+  alert('Welcome to Code Writers');
+});
+
+document.querySelector(".portfolio").addEventListener("click",function(){
+    alert("Continue to portfolio?");
+});
+
+document.querySelector(".about").addEventListener("click",function(){
+    alert("Continue to about?");
+});
+document.querySelector(".contacts").addEventListener("click",function(){
+    alert("Continue to contacts?");
+});
+
